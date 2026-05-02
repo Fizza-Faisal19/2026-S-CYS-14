74c5235cf68f8bec375a4c28723837f280d2279a
2026-CYS-14 <fizza4112@gmail.com>
2026-05-02 16:30:13 +0500

    Add files via upload

---
+++ Week 8/task 3.py
@@ -0,0 +1,9 @@
+fruits = {
+    "Apple": "سیب",
+    "Mango": "آم",
+    "Banana": "کیلا",
+    "Grapes": "انگور",
+    "Orange": "نارنگی",
+}
+for english , urdu in fruits.items():
+    print(english, "=",urdu)
---
+++ Week 8/task 4.py
@@ -0,0 +1,14 @@
+for i in range(1, 4):
+    for j in range(2, 4):
+        print(f"{i} and {j}")
+
+for i in range(1, 10):
+    for j in range(1, i+1):
+        print("*", end="")
+
+
+
+stars = 10
+for i in range(stars):
+    spaces = stars - i - 1
+    print(' ' * spaces + "*" * (2*i +1))
