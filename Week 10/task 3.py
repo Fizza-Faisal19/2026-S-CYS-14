9d3fec9f12c3909140a53f97f8f59c1bc477b1e1
2026-CYS-14 <fizza4112@gmail.com>
2026-05-24 18:20:46 +0500

    Add files via upload

---
+++ Week 10/task 3.py
@@ -0,0 +1,4 @@
+for i in rage(1, 6):
+    for j in range(i):
+        print("*", end= " ")
+    print()
---
+++ Week 10/task 4.py
@@ -0,0 +1,5 @@
+for i in rage(j):
+    for j in range(1, 5):
+        print("*", end= " ")
+    print()
+    
